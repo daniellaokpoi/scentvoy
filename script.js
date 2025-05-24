cefce4519544724782b3392535885faaa36ea9b8
daniellaokpoi <daniellaokpoi@gmail.com>
2025-05-24 04:35:20 +0100

    corrected anchor tags for perfumes

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ shopLink.addEventListener('mouseover', function(){
     
   }
 })
+
+// Add to Wishlist
+let wishBtn = document.querySelectorAll('.wishlist');
+let wishIcon = document.getElementById('wishlist-icon');
+
+wishBtn.forEach((wishButton) => {
+  wishButton.addEventListener('click', () => {
+    if (wishIcon.style.color === 'red'){
+      wishIcon.style.color = 'black';
+    }else {
+      wishIcon.style.color = 'red';
+    }
+  })
+});
+
+
+// controll for the the aleart
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you liked this Fragrance!', 'success')
+  })
+}else {
+  appendAlert('Nice, you unliked this Fragrance!', 'success')
+}
